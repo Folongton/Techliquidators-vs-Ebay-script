47de1956e2bb426d50f9aca35257ca8626ae38d1
Folongton <42916679+Folongton@users.noreply.github.com>
2019-05-22 13:18:40 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,43 +2,43 @@ import sqlite3
 
 
 class Database:
-    def __init__(self, name=None):  #__init__  method (CONSTRUCTOR) requires defined variables when creating new instance(object) out of this class.  https://www.youtube.com/watch?v=KzZhAqu3s4M
-        self.conn = None           # https://stackoverflow.com/questions/8985806/python-constructors-and-init
-        self.cursor = None         # None is null in Python. Why name=None ??? --> https://graysonkoonce.com/always-use-none-for-default-args-in-python/
-        if name:                   # So, if there is no name was given here -> **db = Database('techliquidators.sqlite')**, then it would do nothing ?
+    def __init__(self, name=None):  
+        self.conn = None           
+        self.cursor = None         
+        if name:                   
             self.open(name)
 
 
 
-    def open(self, name):                          # try/except/finally block see: https://docs.python.org/2.5/whatsnew/pep-341.html
+    def open(self, name):                          
         try:
             self.conn = sqlite3.connect(name)
             self.cursor = self.conn.cursor()
             self.cursor.execute(
                 'create table if not exists items (id int  not null constraint items_pk primary key, added date not null);')
             self.conn.commit()
-        except sqlite3.Error as e:                 #variable 'e' was not used
+        except sqlite3.Error as e:                 
             print("Error connecting to database!")
-            print("Database error: %s" % e)       # I added this line
+            print("Database error: %s" % e)       
         finally:
             self.delete_trash()
 
     def close(self):
-        if self.conn:              #  Do we need 'if' statement. We can do 3 lines after and that's all, doesn't we ?
-            self.conn.commit()     # this line is not necessary ? or we need it in case ?
+        if self.conn:              
+            self.conn.commit()     
             self.cursor.close()
             self.conn.close()
 
     def get(self, table, columns, limit=None):
-        query = f"SELECT {columns} from {table};"        # formatted string literals -> https://realpython.com/python-f-strings/#f-strings-a-new-and-improved-way-to-format-strings-in-python
+        query = f"SELECT {columns} from {table};"        
         self.cursor.execute(query)
-        rows = self.cursor.fetchall()                    # Takes all rows (auction numbers) which are already ni DB
-        return rows[len(rows) - limit if limit else 0:]  # python list slicing
+        rows = self.cursor.fetchall()                   
+        return rows[len(rows) - limit if limit else 0:]  
 
     def insert_many(self, table, items):
-        if items:                                                        # for all items
-            items = [f'({item},  date("now"))' for item in items]        # (New python 3.6 formating f') Adds NOW date to all Auction numbers
-            query = "INSERT INTO {0} (ID, ADDED) VALUES {1}".format(table, ','.join(items))  # Older formating
+        if items:                                                       
+            items = [f'({item},  date("now"))' for item in items]        l
+            query = "INSERT INTO {0} (ID, ADDED) VALUES {1}".format(table, ','.join(items))  
             # query = "INSERT INTO " + table + " (ID, ADDED) VALUES "
             self.cursor.execute(query)
             self.conn.commit()
