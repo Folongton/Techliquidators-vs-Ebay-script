3fb4c93b7ae19eec5165a428e5c098326fc80862
Folongton <42916679+Folongton@users.noreply.github.com>
2019-05-22 13:19:48 -0500

    Update ebay.py

--- ebay.py
+++ ebay.py
@@ -1,13 +1,13 @@
-import os           # we don't need it here, do we ?
+import os           
 
 import requests
 import json
 
 
-# print(endpoint)  # to get to actual request link
+# print(endpoint)  
 
 class Ebay:
-    def __init__(self, apikey):    # initial function requires apikey
+    def __init__(self, apikey):    
         self.apikey = apikey
         self.endpoin  = 'https://svcs.ebay.com/services/search/FindingService/v1' \
            '?OPERATION-NAME=findCompletedItems' \
@@ -22,20 +22,20 @@ class Ebay:
            '&SoldItemsOnly=true'
 
     def search(self, page=1, keyword=None):
-        if keyword and page <= 100:   # what is keyword is true and keyword != ??? # If keyword means keyword is not None and  keyword!=''   Runs until keyword comes from Excel file, when hits '' it stops
+        if keyword and page <= 100:   
             keyword = keyword.replace('&quot;', '')
             keyword = keyword.replace('&', ' ')
             print(f'API Request Keyword: {keyword}, Page fetching: {page}')
             response = requests.get(
-                f'{self.endpoin}&SECURITY-APPNAME={self.apikey}&keywords={keyword}&paginationInput.pageNumber={page}')   # why self.apikey , not apikey ???
-            return self.parse(response.text, page)                # what is Parsing -> https://www.quora.com/What-does-parsing-in-programming-mean
-        else:                                                     # HTML Parsing -> https://stackoverflow.com/questions/20421316/what-does-html-parsing-mean
+                f'{self.endpoin}&SECURITY-APPNAME={self.apikey}&keywords={keyword}&paginationInput.pageNumber={page}')   
+            return self.parse(response.text, page)               
+        else:                                                    
             return False, []
 
-    def parse(self, json_, page):    # format Jason specified in our endpoint. About json decryption/encryption -> https://realpython.com/python-json/
-        has_pages = False            # Need Explanation on a whole block parse...
+    def parse(self, json_, page):     
+        has_pages = False            
         prices = []
-        json_ = json.loads(json_.replace('/**/_cb_findItemsByKeywords(', '')[:-1])  # SLIce NOTATION = replaces begining of json text and returns alltext, but the last character ')' - > https://www.oreilly.com/learning/how-do-i-use-the-slice-notation-in-python
+        json_ = json.loads(json_.replace('/**/_cb_findItemsByKeywords(', '')[:-1])   
         if json_.get('findCompletedItemsResponse'):
             if json_['findCompletedItemsResponse'][0]['ack'][0] == 'Success':
                 if int(json_['findCompletedItemsResponse'][0]['paginationOutput'][0]['totalPages'][0]) > page:
