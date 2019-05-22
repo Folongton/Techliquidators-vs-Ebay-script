affef2a895cc6e38e8946276d358d19bbee47673
Folongton <42916679+Folongton@users.noreply.github.com>
2019-05-22 13:25:15 -0500

    Update consts.py

--- consts.py
+++ consts.py
@@ -2,5 +2,5 @@ import os
 
 cat_url = 'https://techliquidators.com/tl/index.cfm?action=marketplace_main.list&condition=73&pageNumber=1&resultsPerPage=2'
 download_url = 'https://techliquidators.com/tl/index.cfm?action=marketplace_main.spreadsheet'
-directory = os.path.join(os.curdir, 'files')  # os.curdir - shows current directory. os.path.join - joins current directory+folder'files' ( creates folder'files' if absnt.
+directory = os.path.join(os.curdir, 'files')  
 cache_db = 'cache.db'
